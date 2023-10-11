e5d712a082705fa960e3a96a323953a4a552a6ae
Igor Silva Santos <igor.silva1@aluno.ifsp.edu.br>
2023-10-11 10:48:55 -0300

    Terminado a NavBar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,40 @@
-document.getElementById('hambIcon').addEventListener('click', function() {
- document.getElementById('navBar').className = "navbar active"
-})
+/*Variáveis Navbar*/
 
-document.getElementById('closeIcon').addEventListener('click', function() {
-    document.getElementById('navBar').className = "navbar"
+   const navOpenBtn = document.querySelector('[data-navbar-open]');
+   const navCloseBtn = document.querySelector('[data-navbar-close]')
+   const navBar = document.querySelector('[data-navbar]');
+   const overlay = document.querySelector('[data-overlay]');
+
+   const nav = [navOpenBtn, navCloseBtn, overlay]
+
+  for (let i = 0; i < nav.length; i++){
+    nav[i].addEventListener('click', function(){
+      navBar.classList.toggle('active')
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /* navOpenBtn.addEventListener('click', function() {
+    navBar.className = "navbar active"
    })
+   
+   navCloseBtn.addEventListener('click', function() {
+       navBar.className = "navbar"
+      })*/
