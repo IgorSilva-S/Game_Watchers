989f11e06ef62b70dc794f54ade41d2464661191
Igor Silva Santos <igor.silva1@aluno.ifsp.edu.br>
2023-10-11 08:59:32 -0300

    Continuando o projeto

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('hambIcon').addEventListener('click', function() {
+ document.getElementById('navBar').className = "navbar active"
+})
+
+document.getElementById('closeIcon').addEventListener('click', function() {
+    document.getElementById('navBar').className = "navbar"
+   })
